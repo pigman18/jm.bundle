f381871412d3a48b43aca1b4e541908b4bdf427d
pigman18 <a2862424470@gmail.com>
2026-06-16 17:17:56 +0800

    feat: jm.bundle
    1、修复/file代理失败BUG

--- jm.bundle.front/vite.config.ts
+++ jm.bundle.front/vite.config.ts
@@ -1,27 +1,33 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:47310/',
-        ws: true,
-        changeOrigin: true,
-        rewrite: (p) => p.replace(/^\/api/, 'api'),
-      },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:47310/',
+                ws: true,
+                changeOrigin: true,
+                rewrite: (p) => p.replace(/^\/api/, 'api'),
+            },
+            '/file': {
+                target: 'http://127.0.0.1:47310/',
+                ws: true,
+                changeOrigin: true,
+                rewrite: (p) => p.replace(/^\/file/, 'file'),
+            },
+        },
     },
-  },
-  plugins: [
-    vue(),
-    Components({ resolvers: [NaiveUiResolver()], dts: false }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    plugins: [
+        vue(),
+        Components({resolvers: [NaiveUiResolver()], dts: false}),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
 })
